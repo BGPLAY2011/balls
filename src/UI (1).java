bb3c85ac88c526d5b0cb30b134efb41b99d65825
Milfiez <matwey.shemonaew@gmail.com>
2025-04-24 17:39:07 +0300

    first commit

---
+++ src/UI (1).java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.List;
+
+class UI extends JFrame implements MouseListener {
+
+    private final List<MovingCircle> circles = new ArrayList<>();
+
+    public UI() {
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        addMouseListener(this);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+
+        // Рисуем все круги
+        for (MovingCircle circle : circles) {
+            circle.paint(g);
+        }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        // Создаём новый круг при каждом клике
+        MovingCircle newCircle = new MovingCircle(e.getX(), e.getY(), this);
+        circles.add(newCircle);
+        newCircle.start();
+    }
+
+    @Override public void mousePressed(MouseEvent e) {}
+    @Override public void mouseReleased(MouseEvent e) {}
+    @Override public void mouseEntered(MouseEvent e) {}
+    @Override public void mouseExited(MouseEvent e) {}
+}
